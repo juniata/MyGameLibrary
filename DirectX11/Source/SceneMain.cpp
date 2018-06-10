79e8a687d41e09aec918ccdc08f175ca2737d4b7
KTYN <shinsuke199573k@gmail.com>
2018-06-10 20:23:20 +0900

    gitignore追記、弾幕のテスト。

--- DirectX11/Source/SceneMain.cpp
+++ DirectX11/Source/SceneMain.cpp
@@ -1,5 +1,5 @@
 #include	"DX_LIBRARY\DX_Library.h"
-#include	"Bullet.h"
+#include	"BulletManager.h"
 #include	"SceneMain.h"
 #include	"user_helper_class\OGGManager.h"
 
@@ -23,7 +23,7 @@ SceneMain::~SceneMain()
 	DELETE_OBJ(g_tex);
 	DELETE_OBJ(g_tex2);
 	DELETE_OBJ(g_pView);
-	DELETE_OBJ(bullet);
+	DELETE_OBJ(bulletManager);
 }
 //-----------------------------------------------------------------------------------------
 //
@@ -35,7 +35,7 @@ bool SceneMain::Initialize()
 	g_pView = new DX_View();
 	g_tex = new DX_2DObject("14_s2bgay4bpz.jpg");
 	g_tex2 = new DX_2DObject("14_s2bgay4bpz.jpg");
-	bullet = new Bullet();
+	bulletManager = new BulletManager();
 	DX_Lighting::Initialize();
 	//OGGManager::LoadOGG(0, "Resource\\Sound\\1-0004370502.320.ogg");
 	//OGGManager::Play(0);
@@ -52,7 +52,7 @@ bool SceneMain::Update()
 {
 	g_pView->FreeCamera(2.0f);
 
-	bullet->Update();
+	bulletManager->Update();
 
 	return true;
 }
@@ -67,6 +67,6 @@ void SceneMain::Render()
 	g_pView->Clear();
 	g_pView->Active();
 
-	bullet->Render();
+	bulletManager->Render();
 	g_tex->Render(tagRect(0, 0, DX_System::GetWindowWidth(), DX_System::GetWindowHeight()));
 }
--- DirectX11/Source/SceneMain.h
+++ DirectX11/Source/SceneMain.h
@@ -1,12 +1,12 @@
 #ifndef __SCENE_MAIN_H_
 #define __SCENE_MAIN_H_
 
-class Bullet;
+class BulletManager;
 
 class SceneMain : public DX_Scene
 {
 private:
-	Bullet* bullet;
+	BulletManager* bulletManager;
 public:
 	SceneMain();
 	~SceneMain();
